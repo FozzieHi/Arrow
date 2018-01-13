eca52f0d51e6a64af1a3ea1eba1bdcc3f437be69
George <fozzziedoesmc@gmail.com>
2018-01-13 08:54:33 +0000

    Update guildCreate.js

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -6,7 +6,7 @@ const Try = require('../utility/Try.js');
 
 client.on('guildCreate', async (guild) => {
   (async () => {
-    const mainChannel = guild.mainChannel;
+    const mainChannel = guild.defaultChannel;
 
     if (mainChannel !== undefined) {
       return Try(Sender.send(mainChannel, 'Hey! I am Arrow, ya know, the slickest bot in town.\n\nJust wanted to let you know that you can use the `' + Constants.prefix +'help` command to get all the command info a man\'s heart could desire.\n\nIf you have any questions or concerns, you may always join the **Official Arrow Support Server:** ' + Constants.data.links.serverInvite + '.'));
