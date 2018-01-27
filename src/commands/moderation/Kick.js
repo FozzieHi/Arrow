3be0fd8a738a705f65258329eeb3bfa73cffb77d
Fozzie <fozzziedoesmc@gmail.com>
2018-01-27 15:35:58 +0000

    Fix reply message.

--- src/commands/moderation/Kick.js
+++ src/commands/moderation/Kick.js
@@ -33,7 +33,7 @@ class Kick extends patron.Command {
 
   async run(msg, args, sender) {
     await args.member.kick(args.reason);
-    await sender.reply('Successfully kicked ' + args.member.user.tag.boldify() + '.');
+    await sender.reply('Successfully kicked ' + StringUtil.boldify(args.member.user.tag) + '.');
     await sender.dm(args.member.user, StringUtil.boldify(msg.author.tag) + ' has kicked you' + (StringUtil.isNullOrWhiteSpace(args.reason) ? '.' : ' for the reason: ' + args.reason + '.'), { guild: msg.guild });
     await db.userRepo.upsertUser(args.member.id, msg.guild.id, { $inc: { kicks: 1 } });
     return ModerationService.tryModLog(msg.dbGuild, msg.guild, 'Kick', Constants.kickColor, args.reason, msg.author, args.member.user);
