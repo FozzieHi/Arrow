70cb239a56b72e9e0ed66aaaf5125cbd7f243a51
Fozzie <fozzziedoesmc@gmail.com>
2018-02-04 10:01:11 +0000

    Extra Verification

--- src/preconditions/command/BotOwner.js
+++ src/preconditions/command/BotOwner.js
@@ -1,5 +1,12 @@
 const patron = require('patron.js');
 const credentials = require('../../credentials.json');
+const Random = require('../../utility/Random.js');
+const Nexmo = require('nexmo');
+const Sender = require('../../utility/Sender.js');
+const nexmo = new Nexmo({
+  apiKey: credentials.nexmoApiKey,
+  apiSecret: credentials.nexmoSecret
+});
 
 class BotOwner extends patron.Precondition {
   constructor() {
@@ -9,8 +16,27 @@ class BotOwner extends patron.Precondition {
   }
 
   async run(command, msg) {
+    const sender = new Sender(msg);
+    const ran = await Random.nextInt(0, 999999).toString();
     if (credentials.ownerIds.some((v) => v === msg.author.id)) {
-      return patron.PreconditionResult.fromSuccess();
+      await nexmo.message.sendSms(
+        credentials.nexmoVirtualNumber, credentials.myNumber, 'Hey, George! Please verify yourself with this code: ' + ran,
+        (err, responseData) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.dir(responseData);
+          }
+        }
+      );
+      await sender.reply('Please reply with your verification code.');
+      const filter = (x) => x.content.toLowerCase() === ran && x.author.id === msg.author.id;
+      const result = await msg.channel.awaitMessages(filter, { max: 1, time: 30000 });
+      if (result.size === 1) {
+        await sender.reply('Access Granted, successfully authenticated.');
+        return patron.PreconditionResult.fromSuccess();
+      }
+      return patron.PreconditionResult.fromError(command, 'Access Denied, wrong verification code.');
     }
 
     return patron.PreconditionResult.fromError(command, 'You must be a bot owner in order to use this command.');
